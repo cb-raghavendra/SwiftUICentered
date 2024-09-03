2bc6a50b74b409a11d956cb1070cc899e82d1f27
cb-raghavendra <raghavendra.s@cicbuzz.com>
2024-09-03 11:07:28 +0530

    Commit

--- Sources/SwiftUICentered/SwiftUICentered.swift
+++ Sources/SwiftUICentered/SwiftUICentered.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+
+extension View {
+    public func centerHorizontally() -> some View {
+        HStack {
+            Spacer()
+            self
+            Spacer()
+        }
+    }
+}
